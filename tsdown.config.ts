1e6c7a5fc919f5440f4ab6f9b818a2b6d9a67cf9
Jason Godesky <jefgodesky@gmail.com>
2026-07-13 17:31:23 -0400

    Add tsdown config

---
+++ tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, type UserConfig } from 'tsdown'
+
+const config: UserConfig = defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  dts: true,
+  clean: true,
+  publint: true,
+  attw: {profile: 'esm-only'},
+})
+
+export default config
