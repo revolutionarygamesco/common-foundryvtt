efa72dd89ac467a66c4d32f1a9f763ab41efd7cc
Jason Godesky <jefgodesky@gmail.com>
2026-07-14 21:42:45 -0400

    rollTable: Remove type assertion

--- src/wrappers/roll-table.ts
+++ src/wrappers/roll-table.ts
@@ -2,6 +2,8 @@ export const rollTable = async (
   uuid: string,
   options: object = { displayChat: false }
 ): Promise<{ roll: foundry.dice.Roll, results: foundry.documents.TableResult[] } | null> => {
-  const table = await foundry.utils.fromUuid(uuid) as foundry.documents.RollTable | null
-  return table ? await table.draw(options) : null
+  const table = await foundry.utils.fromUuid(uuid)
+  return typeof (table as foundry.documents.RollTable)?.draw === 'function'
+    ? await (table as foundry.documents.RollTable).draw(options)
+    : null
 }
