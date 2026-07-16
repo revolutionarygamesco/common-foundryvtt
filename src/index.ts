413604c717ed2b30be7c65d5b3552c9761f596de
Jason Godesky <jefgodesky@gmail.com>
2026-07-16 18:19:44 -0400

    Export Linkable

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export { generateApplicationPosition } from './utilities/app-pos.ts'
 export { getDroppedDocument } from './utilities/dropped-document.ts'
 export { generateID } from './utilities/generate-id.ts'
 export { getID } from './utilities/get-id.ts'
-export { makeLink } from './utilities/make-link.ts'
+export { makeLink, type Linkable } from './utilities/make-link.ts'
 
 export { drawDescription } from './wrappers/draw-description.ts'
 export { drawName } from './wrappers/draw-name.ts'
