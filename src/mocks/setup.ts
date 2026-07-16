f7008d1f67f0115946e530cfec396e0385eaa239
Jason Godesky <jefgodesky@gmail.com>
2026-07-16 17:31:50 -0400

    Mock escapeHTML

--- src/mocks/setup.ts
+++ src/mocks/setup.ts
@@ -1,5 +1,13 @@
 type Dict = Record<string, unknown>
 
+const escapeHTML = (original: string): string => {
+  return original.replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+}
+
 const globals = globalThis as Dict
 const game = (globals.game ?? {}) as Dict
 const i18n = (game.i18n ?? {}) as Dict
@@ -12,6 +20,7 @@ const foundry = (globals.foundry ?? {}) as Dict
 const utils = (foundry.utils ?? {}) as Dict
 
 utils.deepClone = <T extends object>(original: T): T => structuredClone(original)
+utils.escapeHTML = escapeHTML
 utils.fromUuid = async (uuid: string): Promise<foundry.abstract.Document | null> => null
 foundry.utils = utils
 globals.foundry = foundry
