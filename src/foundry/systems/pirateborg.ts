de7e80278f2bca2d0a91c6219e06d36c0b973797
Jason Godesky <jefgodesky@gmail.com>
2026-07-25 10:30:33 -0400

    Expanded system definitions for Pirate Borg

--- src/foundry/systems/pirateborg.ts
+++ src/foundry/systems/pirateborg.ts
@@ -1,19 +1,84 @@
-export interface PirateBorgActorSystem {
-  attributes?: {
-    speed?: { value: number }
-    crew?: { min: number; max: number; value: number }
-    cargo?: { max: number; value: number }
-    featureType?: string
+export namespace PirateBorgSystem {
+  export type Die = 'd2' | 'd4' | 'd6' | 'd8' | 'd10' | 'd12' | 'd20'
+
+  export interface Stat {
+    value: number
   }
-  captain?: string
-  crews?: string[]
-  silver?: number
+
+  export interface Meter extends Stat {
+    max: number
+  }
+
+  export interface Range extends Meter {
+    min: number
+  }
+
+  export interface ShipWeapon {
+    die: Die
+    quantity?: number
+    warning?: string
+  }
+
+  export interface CreatureWeapon {
+    formula: Die
+    description?: string
+  }
+
+  export interface Actor {
+    abilities?: {
+      agility?: Stat
+      presence?: Stat
+      skill?: Stat
+      spirit?: Stat
+      strength?: Stat
+      toughness?: Stat
+    }
+    attributes?: {
+      armor?: CreatureWeapon
+      attack?: CreatureWeapon
+      cargo?: Meter
+      carryingModifier?: Stat
+      combat?: {
+        armorTierModifier?: number
+        attackModifier?: number
+        damageModifier?: number
+        drModifier?: number
+        initiativeModifier?: number
+        luckyModifier?: number
+        speedModifier?: number
+      }
+      crew?: Range
+      extraResource?: Range
+      grog?: {
+        drinks?: number
+        hoursRemaining?: number
+      }
+      hp?: Meter
+      hull?: Range
+      luck?: Range
+      morale?: number
+      rituals?: Range
+      shanties?: Range
+      speed?: Range
+    }
+    baseClass?: string
+    description?: string
+    weapons?: {
+      smallArms?: ShipWeapon
+      broadsides?: ShipWeapon
+      ram?: ShipWeapon
+    }
+    captain?: string
+    crews?: string[]
+    silver?: number
+    special?: string
+  }
 }
 
 declare global {
   namespace foundry {
     namespace documents {
-      interface ActorSystem extends PirateBorgActorSystem {}
+      interface ActorSystem extends PirateBorgSystem.Actor {}
     }
   }
 }
