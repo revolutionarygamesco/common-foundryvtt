cdc6c9d52bf646337e843f71f1a4e73b0f3875af
Jason Godesky <jefgodesky@gmail.com>
2026-08-01 17:23:56 -0400

    There is no foundry.fields; it's foundry.data.fields
    Don't believe everything you read in the docs.

---
+++ src/foundry/data.ts
@@ -0,0 +1,35 @@
+export {}
+
+declare global {
+  namespace foundry {
+    namespace data {
+      namespace fields {
+        class StringField {
+          constructor (options?: object, context?: object)
+        }
+
+        class ColorField extends StringField {}
+        class DocumentIdField extends StringField {}
+        class ForeignDocumentIdField extends DocumentIdField {}
+        class DocumentUUIDField extends StringField {}
+        class FilePathField extends StringField {}
+        class HTMLField extends StringField {}
+        class JSONField extends StringField {}
+        class JavaScriptField extends StringField {}
+
+        class DocumentTypeField extends StringField {
+          constructor (documentClass?: object, options?: object, context?: object)
+        }
+
+        class NumberField {
+          constructor (options?: object, context?: object)
+        }
+
+        class AlphaField extends NumberField {}
+        class AngleField extends StringField {}
+        class HueField extends StringField {}
+        class IntegerSortField extends StringField {}
+      }
+    }
+  }
+}
