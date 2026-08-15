016cd72ea99f1c7dfd8c9d856bb017d1b74a2f98
Jason Godesky <jefgodesky@gmail.com>
2026-08-15 17:28:22 -0400

    Add game.items

--- src/foundry/globals.ts
+++ src/foundry/globals.ts
@@ -23,6 +23,7 @@ declare global {
       }
       actors: foundry.utils.Collection<string, foundry.documents.Actor>
       folders: foundry.utils.Collection<string, foundry.documents.Folder>
+      items: foundry.utils.Collection<string, foundry.documents.Item>
       journal: foundry.utils.Collection<string, foundry.documents.JournalEntry>
       modules: foundry.utils.Collection<string, foundry.packages.Module>
       packs: foundry.utils.Collection<string, any>
